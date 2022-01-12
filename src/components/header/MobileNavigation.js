c2c31a9964c97b222e2459b4d7982896205a7b83
Uson uulu Mairambek <usonuulu.mairambek.pi2@gmail.com>
2022-01-12 23:38:05 +0600

    fix burger menu

--- src/components/header/MobileNavigation.js
+++ src/components/header/MobileNavigation.js
@@ -4,15 +4,13 @@ import "./header.css";
 import { HiMenuAlt1 } from "react-icons/hi";
 import { GrClose } from "react-icons/gr";
 function MobileNavigation() {
-  const [open, setopen] = useState(true);
+  const [open, setopen] = useState(false);
   return (
     <div className="mobile__navigation">
-        {open && <Navlinks />}
-
-      {!open ? (
+      {open && <Navlinks />}      {!open ? (
         <HiMenuAlt1 size="40px" onClick={() => setopen(!open)} />
       ) : (
-        <GrClose size="40px" onClick={() => setopen(!open)} />
+        <GrClose size="37px" onClick={() => setopen(!open)} />
       )}
     </div>
   );
