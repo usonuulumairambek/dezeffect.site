6b8329dafa641763cb80326b3a90a2e3c79056e7
Uson uulu Mairambek <usonuulu.mairambek.pi2@gmail.com>
2022-01-12 19:05:41 +0600

    burger menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,48 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Burger from "./Burger";
 import "./header.css";
 import logo from "./logo.svg";
-import burger from "./menu.png"
 function Header() {
+  const [bugeractive, setbugeractive] = useState(false)
   return (
     <div className="header">
       <div className="header__content">
         <div className="logo">
           <img src={logo} alt="" />
           <div> </div>
-          <p className="logo-text" >
-          Дезинфекция <br /> Бишкек
+          <p className="logo-text">
+            Дезинфекция <br /> Бишкек
           </p>
         </div>
         <div className="navbar">
           <ul className="navbar__nav">
             <li className="nav-item">
               <a className="nav-link" href="#">
-              Главная
+                Главная
               </a>
             </li>
-            
+
             <li className="nav-item">
               <a className="nav-link" href="#">
-              Химчистка
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-              График
+                Химчистка
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-              Услуги
+                График
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Услуги
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link disabled" href="#">
-              Контакты
+                Контакты
               </a>
             </li>
           </ul>
-          <img className="menu" src={burger} alt="" />
+          <Burger active={bugeractive} btn={setbugeractive} />
         </div>
       </div>
     </div>
